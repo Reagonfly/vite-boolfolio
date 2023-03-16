66e306287af48a71787fb493942f0993be828f9c
Reagonfly <reagon@hotmail.it>
2023-03-16 23:34:54 +0100

    im really done atm

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -21,11 +21,16 @@ export default {
                 surname: this.surname,
                 email: this.email,
                 phone: this.phone,
-                message: this.message
+                message: this.message,
+                success: false,
             }
+
+            this.loading = true;
+
             axios.post(`${this.store.baseUrl}/api/contacts`, data).then((response) => {
                 if(!this.success){
                     this.errors = response.data.errors
+                    this.loading = false;
                 }
                 else{
                     this.name = '';
@@ -33,6 +38,9 @@ export default {
                     this.email = '';
                     this.phone = '';
                     this.message = '';
+                    this.success = true;
+                    this.loading = false;
+                    
                 }
             });
         }
@@ -43,6 +51,9 @@ export default {
 <template lang="">
     <div class="container my-2">
         <div class="row">
+            <div class="col-12 text-center" v-if="success">
+                Message sent Correctly
+            </div>
             <div class="col-12">
                 <h2 class="text-center">Contacts Page</h2>
             </div>
@@ -74,25 +85,40 @@ export default {
                         <div class="col-12 col-md-6 my-2">
                             <label class="control-label" for="name">Name</label>
                             <input type="text" class="form-control" name="name" id="name" placeholder="Name" v-model="name">
+                            <div v-if="errors != null" v-for="(error, index) in errors.name" :key="`message-error-${index}`" class="text-danger">
+                                {{ error }}
+                            </div>
                         </div>
                         <div class="col-12 col-md-6 my-2">
                             <label class="control-label" for="surname">Surname</label>
                             <input type="text" class="form-control" name="surname" id="surname" placeholder="Surname" v-model="surname">
+                            <div v-if="errors != null" v-for="(error, index) in errors.surname" :key="`message-error-${index}`" class="text-danger">
+                                {{ error }}
+                            </div>
                         </div>
                         <div class="col-12 col-md-6 my-2">
                             <label class="control-label" for="email">Email</label>
                             <input type="mail" class="form-control" name="email" id="email" placeholder="Email" v-model="email">
+                            <div v-if="errors != null" v-for="(error, index) in errors.email" :key="`message-error-${index}`" class="text-danger">
+                                {{ error }}
+                            </div>
                         </div>
                         <div class="col-12 col-md-6 my-2">
                             <label class="control-label" for="phone">Phone nr.</label>
                             <input type="phone" class="form-control" name="phone" id="phone" placeholder="Phone nr." v-model="phone">
+                            <div v-if="errors != null" v-for="(error, index) in errors.phone" :key="`message-error-${index}`" class="text-danger">
+                                {{ error }}
+                            </div>
                         </div>
                         <div class="col-12 my-2">
                             <label class="control-label" for="message">Write your Message here</label>
                             <textarea class="form-control" name="message" id="message" placeholder="Message" v-model="message"></textarea>
+                            <div v-if="errors != null" v-for="(error, index) in errors.message" :key="`message-error-${index}`" class="text-danger">
+                                {{ error }}
+                            </div>
                         </div>
                         <div class="col-12 my-2">
-                            <button type="submit" class="btn btn-sm btn-secondary px-3">Send</button>
+                            <button type="submit" class="btn btn-sm btn-secondary px-3" :disabled="loading">{{ loading ? 'sending...' : 'Send'}}</button>
                         </div>
                     </div>
                 </form>
